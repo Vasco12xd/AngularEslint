900fc3cfa111cf8fbd1562d6e7eaf405850026f6
Esteban Vasco <evasco.27@hotmail.com>
2022-02-17 14:11:24 -0500

    Commit inicial
    Se agregar los archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {FocusMonitor, FocusOrigin} from '@angular/cdk/a11y';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  NgZone,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 
+/** @title Focusing with a specific FocusOrigin */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app.component',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  title = 'boilerplate';
+export class FocusMonitorFocusViaExample implements OnDestroy, AfterViewInit {
+  @ViewChild('monitored') monitoredEl!: ElementRef<HTMLElement>;
+
+  origin = this.formatOrigin(null);
+
+  constructor(
+    public focusMonitor: FocusMonitor,
+    private _cdr: ChangeDetectorRef,
+    private _ngZone: NgZone,
+  ) {}
+
+  ngAfterViewInit() {
+    this.focusMonitor.monitor(this.monitoredEl).subscribe(origin =>
+      this._ngZone.run(() => {
+        this.origin = this.formatOrigin(origin);
+        this._cdr.markForCheck();
+      }),
+    );
+  }
+
+  ngOnDestroy() {
+    this.focusMonitor.stopMonitoring(this.monitoredEl);
+  }
+
+  formatOrigin(origin: FocusOrigin): string {
+    return origin ? origin + ' focused' : 'blurred';
+  }
 }
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
